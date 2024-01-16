d9a0345de2bc50c92f613c5c847b04f8f91bcc51
Sanidhya <messi.god1029@gmail.com>
2024-01-17 00:28:13 +0530

    renamed app.js to index.js

--- index.js
+++ index.js
@@ -97,6 +97,11 @@ app.get('/', (req,res) => {
     res.render('home');
 })
 
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
